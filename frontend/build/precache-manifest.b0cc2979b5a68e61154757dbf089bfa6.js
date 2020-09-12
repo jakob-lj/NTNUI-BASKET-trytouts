a45f1f3d72555df0ea39f81d38257c7e4cd86db5
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-09-13 01:30:31 +0200

    added build from local

---
+++ frontend/build/precache-manifest.b0cc2979b5a68e61154757dbf089bfa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d198452667238d58b3322188844e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d80ce58306cf455992f4",
+    "url": "/static/css/main.ef0da511.chunk.css"
+  },
+  {
+    "revision": "2ec8689e177e997fdb8e",
+    "url": "/static/js/2.92affbaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92affbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80ce58306cf455992f4",
+    "url": "/static/js/main.4320ce72.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
