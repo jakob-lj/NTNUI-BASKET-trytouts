23c9be0384e18e108ffa750167b943d088ee9e03
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-09-13 00:24:12 +0200

    finished

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+
+
+const express = require('express')
+var bodyParser = require('body-parser')
+
+const { Client } = require('pg')
+const client = new Client()
+
+client.connect()
+
+const cors = require('cors')
+const app = express()
+
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.get('/api/db', async (req, res) => {
+    let teamId = ''
+    try {
+        const r = await client.query('select t.id, t.time, t.date, team.name, count(j.id) as availableSpots from times as t inner join teams as team on team.id = t.teamid left join joining as j on t.id = j.tid where t.teamid = $1::uuid group by t.id, t.time, team.name, t.date', [teamId])
+        
+        res.send(r.rows)
+
+    } catch (e) {
+        res.send({error: true})
+    }
+    
+})
+
+
+app.get('/api/team/:teamId/', async (req, res) => {
+    let teamId = req.params.teamId
+    try {
+        const r = await client.query('select t.id, t.time, t.date, team.name, (select availablespots from times where times.id = t.id)-count(j.id) as count from times as t inner join teams as team on team.id = t.teamid left join joining as j on t.id = j.tid where t.teamid = $1::uuid group by t.id, t.time, team.name, t.date', [teamId])
+        res.send(r.rows)
+
+    } catch (e) {
+        res.send({error: true})
+    }
+})
+
+app.post('/api/attend/:timeId/', async (req, res) => {
+    const {timeId, name, email, phone} = req.body
+
+
+    if (timeId !== '' && name !== '' && email !== '' && phone !== ''&& timeId !== undefined && name !== undefined && email !== undefined && phone !== undefined) {
+
+        const checkToSeeIfAvailable = await client.query('select (select availablespots from times where times.id = joining.tid) - count(joining.id) as count from joining where joining.tid = $1::uuid group by joining.tid', [timeId])
+        if (checkToSeeIfAvailable.rows.length === 0 || checkToSeeIfAvailable.rows[0].count > 0) {
+            const result = await client.query('insert into joining(tid, name, email, phone) values ($1::uuid, $2::text, $3::text, $4::text) RETURNING *', [timeId, name, email, phone])
+            const retResult = await client.query('select * from times where id = $1::uuid', [result.rows[0].tid])
+    
+            res.send({...result.rows[0], ...retResult.rows[0]})
+
+        } else {
+            return res.send({error: true, status: 'Fult'})
+        }
+    } else {
+        res.send({error: true})
+    }
+})
+
+app.listen(8000, () => {
+    console.log('Listening at 8000')
+})
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,147 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+import './home.css'
+
+const Home = ({match}) => {
+    
+    let backend = 'http://localhost:8000'
+    
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(false)
+    const [modalVisible, setModalVisible] = useState(false)
+    const [event, setEvent] = useState(null)
+    const [registered, setRegistered] = useState(false)
+    const [regData, setRegData] = useState(null)
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const attend = () => {
+        fetch(`${backend}/api/attend/${event}/`, {method: 'POST', headers: {'Content-Type': 'Application/JSON'}, body: JSON.stringify({name, email, phone, timeId: event})}).then(r => r.json()).then(r => {
+            setModalVisible(false)
+            if (r.error) {
+                setError(true)
+            } else {
+                setRegData(r)
+                setRegistered(true)
+            }
+        })
+    }
+
+    const join = (event) => {
+        setModalVisible(true)
+        setEvent(event)
+    }
+    
+    useEffect(() => {
+        try {
+            let teamId = window.location.href.split('?')[1].split('=')[1]        
+            if (teamId === undefined) {
+                throw 'Undefined team'
+            }
+            fetch(`${backend}/api/team/${teamId}/`).then(r => r.json()).then(r => {
+                if (r.error) {
+                    setError(true)
+                } else {
+                    setData(r)
+                }
+            }).catch(err => {
+                console.log(err)
+                setError(true)
+            })
+
+        } catch (e) {
+            console.log(e)
+            setError(true)
+        }
+    }, [])
+    
+    
+
+    let avpos = undefined
+
+    if (data !== null) {
+        avpos = data.map(d => {
+            return <div className={'cont'} key={d.date + d.time}>
+                <h5>{d.teamName}</h5>
+                <p>
+                    {d.date}: {d.time}
+                </p>
+                <p>Ledige plasser: {d.count}</p>
+                
+                <div>
+                    <button onClick={() => join(d.id)}>Meld på</button>
+                </div>
+            </div>
+        })
+    }
+
+    if (modalVisible && event) {
+        return <div>
+                <div>
+                    <button onClick={() => {setModalVisible(false)}}>Tilbake</button>
+                </div>
+                <div className={'wrapper'}>
+                    <h2>Nesten ferdig, vi trenger bare litt mer informasjon!</h2>
+                    <div>
+                        <input type={'text'} value={name} onChange={(e) => setName(e.target.value)} placeholder={'Name'} />
+                    </div>
+                    <div>
+                        <input type={'email'} value={email} onChange={(e) => setEmail(e.target.value)} placeholder={'Email'} />
+                    </div>
+                    <div>
+                        <input type={'text'} value={phone} onChange={(e) => setPhone(e.target.value)} placeholder={'Phone'} />
+                    </div>
+                    <div>
+                        <button onClick={attend}>Meld på</button>
+                    </div>
+                    <p>Dataen vil bli slettet etter tryouts</p>
+                </div>
+        </div>
+        
+    }
+
+    if (registered) {
+        
+        return <div className={'wrapper'}>
+            <h2>Du er nå påmeldt</h2>
+            <p>{regData.email} at {regData.date} kl. {regData.time}</p>
+        </div>
+    }
+
+    return <div>
+        
+        <div className="wrapper">
+        <h1>Velkommen til tryouts med NTNUI Basket</h1>
+
+    </div>
+
+    {error && <div className="wrapper" id="error">
+        <h2>Det skjedde en feil? Kan du sende en mail til <a href="mailto: jakob.johannessen@ntnui.no">jakob.johannessen@ntnui.no</a> </h2>
+    </div>}
+
+    {data === null &! error ?
+    <div className="wrapper" id="content">
+        <p>Vi tar deg straks til siden hvor du kan velge når du vil på tryouts!</p>
+        <p>Loading...</p>
+    </div> : null}
+
+    {data !== undefined &! error ?
+    <div className='wrapper'>
+        <h3 style={{margin: '1em'}}>Velg tid for tryouts</h3>
+        <div className={'midwrap'}>
+            {avpos}
+        </div>
+    </div> : null}
+
+    <div className="wrapper">
+        <p>Made in a rush by <a href="https://ntnui.no/basket">NTNUI Basket</a></p>
+
+    </div>
+    </div>
+}
+
+export default Home
